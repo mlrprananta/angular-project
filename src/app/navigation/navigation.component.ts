00f2888486e5e9aea594470b264091c1373c469f
Luke Prananta <m.l.r.prananta@student.tudelft.nl>
2019-11-09 17:06:32 +0100

    Convert to scss, update nav

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  isOpen: boolean = false;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    console.log("Started")
   }
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    console.debug('Ok')
+  }
+
+  onClick() {
+    this.isOpen = !this.isOpen;
+
+  }
 }
